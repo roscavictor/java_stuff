b61fdd3cce067637482cc106a21a86b8baf846bd
Victor Roşca <49694231+roscavictor@users.noreply.github.com>
2022-08-07 00:14:57 +0300

    Update compareObjects.java

--- watermeters/compareObjects.java
+++ watermeters/compareObjects.java
@@ -4,12 +4,12 @@ package com.company;
 
 import java.util.Comparator;
 
-public class compareObjects implements Comparator<citire> {
+public class compareObjects implements Comparator<reading> {
 
     @Override
-    public int compare(citire obj1, citire obj2) {
-        citire myObj1 = (citire)obj1;
-        citire myObj2 = (citire)obj2;
+    public int compare(reading obj1, reading obj2) {
+        reading myObj1 = (reading)obj1;
+        reading myObj2 = (reading)obj2;
         int result = Integer.compare(myObj1.data%100,myObj2.data%100);
         if (result == 0) {
             // Strings are equal, sort by date
